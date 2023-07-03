e0df14aecc4c970439eb14c1c547be579d9fe3a2
Joshua Owolabi <joshuaoluwasegun99@gmail.com>
2023-07-02 23:11:39 -0700

    Delete app/build directory

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_android_chatmeup_CmuApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.android.chatmeup.CmuApplication"
-)
-public final class _com_android_chatmeup_CmuApplication {
-}
